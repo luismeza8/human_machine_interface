09fc963a124ca2d99ffbf830b994a223b390f838
luismeza8 <angel841bv@gmail.com>
2024-07-12 17:37:12 -0700

    model for medition updated

--- graphs/models.py
+++ graphs/models.py
@@ -2,7 +2,17 @@ from django.db import models
 
 # Create your models here.
 class Medition(models.Model):
-    altitude = models.IntegerField()
-    temperature = models.IntegerField()
+    medition = models.IntegerField(default=0)
+    time = models.IntegerField(default=0)
+    altitude = models.FloatField(default=0.0)
+    temperature = models.FloatField(default=0.0)
+    pressure = models.FloatField(default=0.0)
+    aceleration_x = models.FloatField(default=0.0)
+    aceleration_y = models.FloatField(default=0.0)
+    aceleration_z = models.FloatField(default=0.0)
+    gyro_x = models.FloatField(default=0.0)
+    gyro_y = models.FloatField(default=0.0)
+    gyro_z = models.FloatField(default=0.0)
+
     battery = models.IntegerField()
 
