3568768807ba26128d08262de8abebab8c85013e
luismeza8 <angel841bv@gmail.com>
2024-07-14 15:19:02 -0700

    acelerations and gyros charts now working

--- graphs/consumers.py
+++ graphs/consumers.py
@@ -6,6 +6,31 @@ from channels.generic.websocket import AsyncWebsocketConsumer
 
 from graphs.models import Medition
 
+class PocketQubeConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        await self.accept()
+        last_medition = 0
+
+        while True:
+            medition = await Medition.objects.alast()
+            if last_medition != medition.medition:
+                data = {
+                    'medition': medition.medition,
+                    'altitude': medition.altitude,
+                    'temperature': medition.temperature,
+                    'pressure': medition.pressure,
+                    'aceleration_x': medition.aceleration_x,
+                    'aceleration_y': medition.aceleration_y,
+                    'aceleration_z': medition.aceleration_z,
+                    'gyro_x': medition.gyro_x,
+                    'gyro_y': medition.gyro_y,
+                    'gyro_z': medition.gyro_z,
+                    'battery': medition.battery,
+                }
+                await self.send(json.dumps(data))
+                last_medition = medition.medition
+            await sleep(0.2)
+
 
 class AltitudeConsumer(AsyncWebsocketConsumer):
     async def connect(self):
--- static/js/index.js
+++ static/js/index.js
@@ -65,6 +65,70 @@ const pressure_graph_config = {
   },
 }
 
+const acelerations_graph_config = {
+  type: 'line',
+  data: {
+    labels: new Array(15),
+    datasets: [
+      {
+        label: 'X',
+        data: new Array(15),
+        borderWidth: 4,
+        borderColor: 'rgb(255, 255, 0)',
+      },
+      {
+        label: 'Y',
+        data: new Array(15),
+        borderWidth: 4,
+        borderColor: 'rgb(0, 255, 255)',
+      },
+      {
+        label: 'Z',
+        data: new Array(15),
+        borderWidth: 4,
+        borderColor: 'rgb(0, 255, 0)',
+      },
+    ],
+  },
+  options: {
+    animation: {
+      duration: 1
+    }
+  },
+}
+
+const gyros_graph_config = {
+  type: 'line',
+  data: {
+    labels: new Array(15),
+    datasets: [
+      {
+        label: 'X',
+        data: new Array(15),
+        borderWidth: 4,
+        borderColor: 'rgb(127, 255, 0)',
+      },
+      {
+        label: 'Y',
+        data: new Array(15),
+        borderWidth: 4,
+        borderColor: 'rgb(0, 255, 127)',
+      },
+      {
+        label: 'Z',
+        data: new Array(15),
+        borderWidth: 4,
+        borderColor: 'rgb(0, 127, 0)',
+      },
+    ],
+  },
+  options: {
+    animation: {
+      duration: 1
+    }
+  },
+}
+
 const battery_graph_config = {
   type: 'line',
   data: {
@@ -83,9 +147,12 @@ const battery_graph_config = {
     }
   },
 }
+
 var altitude_chart = new Chart(altitude_canvas, altitude_graph_config);
 var temperature_chart = new Chart(temperature_canvas, temperature_graph_config);
-var pressure_chart = new Chart(pressure_canvas, pressure_graph_config)
+var pressure_chart = new Chart(pressure_canvas, pressure_graph_config);
+var acelerations_chart = new Chart(acelerations_canvas, acelerations_graph_config);
+var gyros_chart = new Chart(gyros_canvas, gyros_graph_config);
 var battery_chart = new Chart(battery_canvas, battery_graph_config);
 
 altitude_socket.onmessage = function(e) {
@@ -139,6 +206,62 @@ pressure_socket.onmessage = function(e) {
   pressure_graph_config.data.datasets[0].data = newGraph;
   pressure_graph_config.data.labels = newLabels;
   pressure_chart.update();
+}
+
+acelerations_socket.onmessage = function(e) {
+  var djangoData = JSON.parse(e.data);
+
+  var new_x_axis = acelerations_graph_config.data.labels;
+  var new_y_axis_x = acelerations_graph_config.data.datasets[0].data;
+  var new_y_axis_y = acelerations_graph_config.data.datasets[1].data;
+  var new_y_axis_z = acelerations_graph_config.data.datasets[2].data;
+
+  new_x_axis.shift();
+  new_x_axis.push(djangoData.medition);
+
+  new_y_axis_x.shift();
+  new_y_axis_x.push(djangoData.x);
+
+  new_y_axis_y.shift();
+  new_y_axis_y.push(djangoData.y);
+  
+  new_y_axis_z.shift();
+  new_y_axis_z.push(djangoData.z);
+
+  acelerations_graph_config.data.labels = new_x_axis;
+  acelerations_graph_config.data.datasets[0].data = new_y_axis_x;
+  acelerations_graph_config.data.datasets[1].data = new_y_axis_y;
+  acelerations_graph_config.data.datasets[2].data = new_y_axis_z;
+
+  acelerations_chart.update();
+}
+
+gyros_socket.onmessage = function(e) {
+  var djangoData = JSON.parse(e.data);
+
+  var new_x_axis = gyros_graph_config.data.labels;
+  var new_y_axis_x = gyros_graph_config.data.datasets[0].data;
+  var new_y_axis_y = gyros_graph_config.data.datasets[1].data;
+  var new_y_axis_z = gyros_graph_config.data.datasets[2].data;
+
+  new_x_axis.shift();
+  new_x_axis.push(djangoData.medition);
+
+  new_y_axis_x.shift();
+  new_y_axis_x.push(djangoData.x);
+
+  new_y_axis_y.shift();
+  new_y_axis_y.push(djangoData.y);
+  
+  new_y_axis_z.shift();
+  new_y_axis_z.push(djangoData.z);
+
+  gyros_graph_config.data.labels = new_x_axis;
+  gyros_graph_config.data.datasets[0].data = new_y_axis_x;
+  gyros_graph_config.data.datasets[1].data = new_y_axis_y;
+  gyros_graph_config.data.datasets[2].data = new_y_axis_z;
+
+  gyros_chart.update();
 }
 
 battery_socket.onmessage = function(e) {
