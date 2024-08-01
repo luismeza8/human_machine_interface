b4708e65f0264334c6ae9cc63baa36f7de867cbc
luismeza8 <angel841bv@gmail.com>
2024-07-31 19:15:17 -0700

    now the remaining tabs have functionality

--- static/js/base.js
+++ static/js/base.js
@@ -33,16 +33,38 @@ pocketqubeSocket.onmessage = function(e) {
 
 const general = document.getElementById('general');
 const atmosfera = document.getElementById('atmosfera');
+const estructura = document.getElementById('estructura');
+const mensajeria = document.getElementById('mensajeria');
 
-const atmosferaBtn = document.getElementById('atmosferaBtn');
 const generalBtn = document.getElementById('generalBtn');
+const atmosferaBtn = document.getElementById('atmosferaBtn');
+const estructuraBtn = document.getElementById('estructuraBtn');
+const mensajeriaBtn = document.getElementById('mensajeriaBtn');
 
-atmosferaBtn.onclick = function() {
+generalBtn.onclick = () => {
+  estructura.classList.add('hidden');
+  atmosfera.classList.add('hidden');
+  mensajeria.classList.add('hidden');
+  general.classList.remove('hidden');
+}
+
+atmosferaBtn.onclick = () => {
+  estructura.classList.add('hidden');
   general.classList.add('hidden');
+  mensajeria.classList.add('hidden');
   atmosfera.classList.remove('hidden');
 }
 
-generalBtn.onclick = function() {
+estructuraBtn.onclick = () => {
+  general.classList.add('hidden');
   atmosfera.classList.add('hidden');
-  general.classList.remove('hidden');
+  mensajeria.classList.add('hidden');
+  estructura.classList.remove('hidden');
+}
+
+mensajeriaBtn.onclick = () => {
+  general.classList.add('hidden');
+  atmosfera.classList.add('hidden');
+  estructura.classList.add('hidden');
+  mensajeria.classList.remove('hidden');
 }
