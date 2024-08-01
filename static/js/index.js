f2098645fa53904c20e32e47eb8f25a4cb1414e5
luismeza8 <angel841bv@gmail.com>
2024-07-31 19:58:59 -0700

    max and min range buttons added for altitude chart

--- static/js/index.js
+++ static/js/index.js
@@ -5,6 +5,14 @@
   const acelerationsCanvas = document.getElementById('acelerations_canvas');
   const gyrosCanvas = document.getElementById('gyros_canvas');
 
+  const altitudeAddMaxRange = document.getElementById('altitude_add_max_range');
+  const altitudeMinusMaxRange = document.getElementById('altitude_minus_max_range');
+  const altitudeAddMinRange = document.getElementById('altitude_add_min_range');
+  const altitudeMinusMinRange = document.getElementById('altitude_minus_min_range');
+
+  const altitudeMaxRange = document.getElementById('altitude_max_range');
+  const altitudeMinRange = document.getElementById('altitude_min_range');
+
   function createChartConfig(datasets, suggestedMin, suggestedMax) {
     return {
       type: 'line',
@@ -30,7 +38,7 @@
       },
     }
   }
-  const altitudeChartConfig = createChartConfig([{data: new Array(15), borderWidth: 4}], 0, 100);
+  const altitudeChartConfig = createChartConfig([{data: new Array(15), borderWidth: 4}], -100, 100);
   const temperatureChartConfig = createChartConfig([{label: 'Temperatura', data: new Array(15), borderWidth: 4, borderColor: 'rgb(255, 0, 0)'}], 20, 30);
   const pressureChartConfig = createChartConfig([{label: 'Presion', data: new Array(15), borderWidth: 4, borderColor: 'rgb(0, 255, 0)'}], 1000, 1100);
   const acelerationsChartConfig = createChartConfig([
@@ -68,6 +76,29 @@
   gyrosChartConfig.data.datasets[1].data = acelerationYValues;
   gyrosChartConfig.data.datasets[2].data = acelerationZValues;
   gyrosChartConfig.data.labels = xAxisValues;
+
+  altitudeMaxRange.innerHTML = altitudeChartConfig.options.scales.y.suggestedMax;
+  altitudeMinRange.innerHTML = altitudeChartConfig.options.scales.y.suggestedMin;
+
+  altitudeAddMaxRange.onclick = () => {
+    altitudeChartConfig.options.scales.y.suggestedMax += 10;
+    altitudeMaxRange.innerHTML = altitudeChartConfig.options.scales.y.suggestedMax;
+  }
+
+  altitudeMinusMaxRange.onclick = () => {
+    altitudeChartConfig.options.scales.y.suggestedMax -= 10;
+    altitudeMaxRange.innerHTML = altitudeChartConfig.options.scales.y.suggestedMax;
+  }
+
+  altitudeAddMinRange.onclick = () => {
+    altitudeChartConfig.options.scales.y.suggestedMin += 10;
+    altitudeMinRange.innerHTML = altitudeChartConfig.options.scales.y.suggestedMin;
+  }
+
+  altitudeMinusMinRange.onclick = () => {
+    altitudeChartConfig.options.scales.y.suggestedMin -= 10;
+    altitudeMinRange.innerHTML = altitudeChartConfig.options.scales.y.suggestedMin;
+  }
 
   setInterval(function() {
     altitudeChart.update();
