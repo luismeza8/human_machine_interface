096dc407c39147078a017481b85e8a95ff478437
luismeza8 <angel841bv@gmail.com>
2024-08-05 18:17:11 -0700

    acelerations and gyro charts are now classes

--- static/js/charts/gyros.js
+++ static/js/charts/gyros.js
@@ -1,64 +1,65 @@
-const gyrosCanvas = document.getElementById('gyros_canvas');
-
-const gyrosAddMaxRange = document.getElementById('gyros_add_max_range');
-const gyrosMinusMaxRange = document.getElementById('gyros_minus_max_range');
-const gyrosAddMinRange = document.getElementById('gyros_add_min_range');
-const gyrosMinusMinRange = document.getElementById('gyros_minus_min_range');
-
-const gyrosMaxRange = document.getElementById('gyros_max_range');
-const gyrosMinRange = document.getElementById('gyros_min_range');
-
-const gyrosChartConfig = {
-  type: 'line',
-  data: {
-    labels: xAxisValues,
-    datasets: [
-      {label: 'X', data: gyroXValues, borderWidth: 4, borderColor: 'rgb(127, 255, 0)'},
-      {label: 'Y', data: gyroYValues, borderWidth: 4, borderColor: 'rgb(0, 255, 127)'},
-      {label: 'Z', data: gyroZValues, borderWidth: 4, borderColor: 'rgb(0, 127, 0)'},
-    ]
-  },
-  options: {
-    scales: {
-      y: {
-        suggestedMin: 0,
-        suggestedMax: 2
-      },
+class GyrosChart {
+  constructor(canvasName) {
+    this.canvas = document.getElementById(canvasName);
+  }
+  chartConfig = {
+    type: 'line',
+    data: {
+      labels: xAxisValues,
+        datasets: [
+          {label: 'X', data: gyroXValues, borderWidth: 4, borderColor: 'rgb(127, 255, 0)'},
+          {label: 'Y', data: gyroYValues, borderWidth: 4, borderColor: 'rgb(0, 255, 127)'},
+          {label: 'Z', data: gyroZValues, borderWidth: 4, borderColor: 'rgb(0, 127, 0)'},
+        ]
     },
-    plugins: {
-      legend: {
-        display: false,
+    options: {
+      scales: {
+        y: {
+          suggestedMin: -1,
+          suggestedMax: 1
+        },
       },
-  },
-    animation: {
-      duration: 1
-    }
-  },
+      plugins: {
+        legend: {
+          display: false,
+        },
+    },
+      animation: {
+        duration: 1
+      }
+    },
+  }
+  getChart() {
+    return new Chart(this.canvas, this.chartConfig);
+  }
+
+  getSuggestedMinimum(chart) {
+    return chart.config.options.scales.y.suggestedMin;
+  }
+
+  getSuggestedMaximum(chart) {
+    return chart.config.options.scales.y.suggestedMax;
+  }
+
+  addToSuggestedMinimum(chart) {
+    chart.config.options.scales.y.suggestedMin += 1;
+    return chart;
+  }
+
+  subtractFromSuggestedMinimum(chart) {
+    chart.config.options.scales.y.suggestedMin -= 1;
+    return chart;
+  }
+
+  addToSuggestedMaximum(chart) {
+    chart.config.options.scales.y.suggestedMax += 1;
+    return chart;
+  }
+
+  subtractFromSuggestedMaximum(chart) {
+    chart.config.options.scales.y.suggestedMax -= 1;
+    return chart;
+  }
 }
 
-const gyrosChart = new Chart(gyrosCanvas, gyrosChartConfig);
 
-gyrosMaxRange.innerHTML = gyrosChartConfig.options.scales.y.suggestedMax;
-gyrosMinRange.innerHTML = gyrosChartConfig.options.scales.y.suggestedMin;
-
-gyrosAddMaxRange.onclick = () => {
-  gyrosChartConfig.options.scales.y.suggestedMax += 5;
-  gyrosMaxRange.innerHTML = gyrosChartConfig.options.scales.y.suggestedMax;
-}
-
-gyrosMinusMaxRange.onclick = () => {
-  gyrosChartConfig.options.scales.y.suggestedMax -= 5;
-  gyrosMaxRange.innerHTML = gyrosChartConfig.options.scales.y.suggestedMax;
-}
-
-gyrosAddMinRange.onclick = () => {
-  gyrosChartConfig.options.scales.y.suggestedMin += 5;
-  gyrosMinRange.innerHTML = gyrosChartConfig.options.scales.y.suggestedMin;
-}
-
-gyrosMinusMinRange.onclick = () => {
-  gyrosChartConfig.options.scales.y.suggestedMin -= 5;
-  gyrosMinRange.innerHTML = gyrosChartConfig.options.scales.y.suggestedMin;
-}
-
-setInterval(function() { gyrosChart.update(); }, 200);
--- static/js/index.js
+++ static/js/index.js
@@ -34,30 +34,89 @@
 
   let temperatureChart = temperatureChartClass.getChart();
 
-  temperatureSuggestedMinimum.innerText = altitudeChartClass.getSuggestedMinimum(altitudeChart);
-  temperatureSuggestedMaximum.innerText = altitudeChartClass.getSuggestedMaximum(altitudeChart);
+  temperatureSuggestedMinimum.innerText = temperatureChartClass.getSuggestedMinimum(temperatureChart);
+  temperatureSuggestedMaximum.innerText = temperatureChartClass.getSuggestedMaximum(temperatureChart);
 
   document.getElementById('general_temperature_add_suggested_minimum_button').onclick = () => {
     temperatureChart = temperatureChartClass.addToSuggestedMinimum(temperatureChart);
     temperatureSuggestedMinimum.innerText = temperatureChartClass.getSuggestedMinimum(temperatureChart);
   }
-  document.getElementById('general_altitude_subtract_suggested_minimum_button').onclick = () => {
-    altitudeChart = altitudeChartClass.subtractFromSuggestedMinimum(altitudeChart);
-    altitudeSuggestedMinimum.innerText = altitudeChartClass.getSuggestedMinimum(altitudeChart);
+  document.getElementById('general_temperature_subtract_suggested_minimum_button').onclick = () => {
+    temperatureChart = temperatureChartClass.subtractFromSuggestedMinimum(temperatureChart);
+    temperatureSuggestedMinimum.innerText = temperatureChartClass.getSuggestedMinimum(temperatureChart);
   }
-  document.getElementById('general_altitude_add_suggested_maximum_button').onclick = () => {
-    altitudeChart = altitudeChartClass.addToSuggestedMaximum(altitudeChart);
-    altitudeSuggestedMaximum.innerText = altitudeChartClass.getSuggestedMaximum(altitudeChart);
+  document.getElementById('general_temperature_add_suggested_maximum_button').onclick = () => {
+    temperatureChart = temperatureChartClass.addToSuggestedMaximum(temperatureChart);
+    temperatureSuggestedMaximum.innerText = temperatureChartClass.getSuggestedMaximum(temperatureChart);
   }
-  document.getElementById('general_altitude_subtract_suggested_maximum_button').onclick = () => {
-    altitudeChart = altitudeChartClass.subtractFromSuggestedMaximum(altitudeChart);
-    altitudeSuggestedMaximum.innerText = altitudeChartClass.getSuggestedMaximum(altitudeChart);
+  document.getElementById('general_temperature_subtract_suggested_maximum_button').onclick = () => {
+    temperatureChart = temperatureChartClass.subtractFromSuggestedMaximum(temperatureChart);
+    temperatureSuggestedMaximum.innerText = temperatureChartClass.getSuggestedMaximum(temperatureChart);
   }
 
   // Temperature Chart end
+  
+  // Acelerations Chart start
+  const acelerationsSuggestedMinimum = document.getElementById('general_acelerations_suggested_minimum');
+  const acelerationsSuggestedMaximum = document.getElementById('general_acelerations_suggested_maximum');
+  const acelerationsChartClass = new AcelerationsChart('general_acelerations_canvas');
+
+  let acelerationsChart = acelerationsChartClass.getChart();
+
+  acelerationsSuggestedMinimum.innerText = acelerationsChartClass.getSuggestedMinimum(acelerationsChart);
+  acelerationsSuggestedMaximum.innerText = acelerationsChartClass.getSuggestedMaximum(acelerationsChart);
+
+  document.getElementById('general_acelerations_add_suggested_minimum_button').onclick = () => {
+    acelerationsChart = acelerationsChartClass.addToSuggestedMinimum(acelerationsChart);
+    acelerationsSuggestedMinimum.innerText = acelerationsChartClass.getSuggestedMinimum(acelerationsChart);
+  }
+  document.getElementById('general_acelerations_subtract_suggested_minimum_button').onclick = () => {
+    acelerationsChart = acelerationsChartClass.subtractFromSuggestedMinimum(acelerationsChart);
+    acelerationsSuggestedMinimum.innerText = acelerationsChartClass.getSuggestedMinimum(acelerationsChart);
+  }
+  document.getElementById('general_acelerations_add_suggested_maximum_button').onclick = () => {
+    acelerationsChart = acelerationsChartClass.addToSuggestedMaximum(acelerationsChart);
+    acelerationsSuggestedMaximum.innerText = acelerationsChartClass.getSuggestedMaximum(acelerationsChart);
+  }
+  document.getElementById('general_acelerations_subtract_suggested_maximum_button').onclick = () => {
+    acelerationsChart = acelerationsChartClass.subtractFromSuggestedMaximum(acelerationsChart);
+    acelerationsSuggestedMaximum.innerText = acelerationsChartClass.getSuggestedMaximum(acelerationsChart);
+  }
+
+  // Acelerations Chart end
+  // Gyros Chart start
+  const gyrosSuggestedMinimum = document.getElementById('general_gyros_suggested_minimum');
+  const gyrosSuggestedMaximum = document.getElementById('general_gyros_suggested_maximum');
+  const gyrosChartClass = new GyrosChart('general_gyros_canvas');
+
+  let gyrosChart = gyrosChartClass.getChart();
+
+  gyrosSuggestedMinimum.innerText = gyrosChartClass.getSuggestedMinimum(gyrosChart);
+  gyrosSuggestedMaximum.innerText = gyrosChartClass.getSuggestedMaximum(gyrosChart);
+
+  document.getElementById('general_gyros_add_suggested_minimum_button').onclick = () => {
+    gyrosChart = gyrosChartClass.addToSuggestedMinimum(gyrosChart);
+    gyrosSuggestedMinimum.innerText = gyrosChartClass.getSuggestedMinimum(gyrosChart);
+  }
+  document.getElementById('general_gyros_subtract_suggested_minimum_button').onclick = () => {
+    gyrosChart = gyrosChartClass.subtractFromSuggestedMinimum(gyrosChart);
+    gyrosSuggestedMinimum.innerText = gyrosChartClass.getSuggestedMinimum(gyrosChart);
+  }
+  document.getElementById('general_gyros_add_suggested_maximum_button').onclick = () => {
+    gyrosChart = gyrosChartClass.addToSuggestedMaximum(gyrosChart);
+    gyrosSuggestedMaximum.innerText = gyrosChartClass.getSuggestedMaximum(gyrosChart);
+  }
+  document.getElementById('general_gyros_subtract_suggested_maximum_button').onclick = () => {
+    gyrosChart = gyrosChartClass.subtractFromSuggestedMaximum(gyrosChart);
+    gyrosSuggestedMaximum.innerText = gyrosChartClass.getSuggestedMaximum(gyrosChart);
+  }
+
+  // Acelerations Chart end
   setInterval(() => {
-    temperatureChart.update();
     altitudeChart.update();
+    temperatureChart.update();
+    acelerationsChart.update();
+    gyrosChart.update();
   }, 200);
 })();
 
